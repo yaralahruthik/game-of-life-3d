9c0ceb70cb418d4518d95a7c0d8711e2524f7f34
yaralahruthik <yaralahruthikreddy@gmail.com>
2023-05-02 13:51:22 +0100

    three fiber and eslint updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Canvas } from '@react-three/fiber';
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="h-screen">
+      <Canvas>
+        <ambientLight intensity={0.1} />
+        <directionalLight color="red" position={[0, 0, 5]} />
+        <mesh>
+          <boxGeometry />
+          <meshStandardMaterial />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
 
 export default App;
